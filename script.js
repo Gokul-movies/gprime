34029f49491a19afc528ea9207a981f5b469eea1
Gokul_movie <74721551+Gokul-movies@users.noreply.github.com>
2022-02-09 21:48:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var $_get = {};
+
+function getParameters() {
+
+  let urlString = window.location.href;
+
+  let paramString = urlString.split('?')[1];
+
+  let queryString = new URLSearchParams(paramString);
+
+  for (let pair of queryString.entries()) {
+
+    //console.log("Key is:" + pair[0]);
+    $_get[pair[0]] = pair[1];
+
+    //console.log("Value is:" + pair[1]);
+
+  }
+
+}
+
